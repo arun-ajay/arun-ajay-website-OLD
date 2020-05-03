6edb893a6efdafb8185a56fd0c7a61c1ba5e76df
arun-ajay <arunajay42795@gmail.com>
2020-05-03 01:13:15 -0400

    editing document meta data

--- src/App.js
+++ src/App.js
@@ -6,8 +6,16 @@ import Projects from "./components/pages/Projects/Projects";
 import Experiences from "./components/pages/experiences/Experiences";
 import { Container, Row, Col } from 'reactstrap';
 import {Route,Switch} from 'react-router-dom';
+import {Helmet} from 'react-helmet'
+
 function App() {
   return (
+    <div>
+      <Helmet>
+        <title>Arun Ajay</title>
+        <meta name="My name is Arun Ajay. I'm an aspiring software engineer with a background in Biomedical Engineering. I'm currently wrapping up my final round of 
+        course for my Computer Science degree. Come visit to check out my latest projects and work experiences!"/>
+      </Helmet>
       <Container fluid className = "main">
           <Row className = "nav">
           <Header></Header>
@@ -21,7 +29,8 @@ function App() {
             </Row>
           </Switch>
       </Container>
+    </div>
+
   );
 }
-
 export default App;
