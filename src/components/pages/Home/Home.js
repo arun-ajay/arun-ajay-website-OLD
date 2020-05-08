316a3f605de7ca54bab43c4cbb99608176aabe9f
arun-ajay <arunajay42795@gmail.com>
2020-05-08 01:30:32 -0400

    merged jumboIntro to home component

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import {Container,Row,Col} from 'reactstrap';
-import JumboIntro from '../../JumboIntro/JumboIntro'
 import "./Home.css";
+import { Jumbotron, Button } from 'reactstrap';
+import {Container,Row,Col} from 'reactstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {mediaData} from "./data"
 
 const Home = (props) => {
 
+    var mediaArray = mediaData.map(d =>(
+        <span>
+          <Button onClick = {() => window.open(d.url,"_blank")} className = "jumboButton"><FontAwesomeIcon icon = {d.icon} size="3x" className = "jumboIcon"/></Button>
+          <div className = "divider"></div>
+        </span>
+        
+      ))
+
         return(
             <div className = "homeDiv">
-                <JumboIntro></JumboIntro>
+                <Jumbotron>
+                    <h1 className = "name">Arun Ajay</h1>
+                    <p className = "title">Engineer and Aspiring Software Developer</p>
+                    <Container>
+                        <Row>
+                            <Col>
+                            {mediaArray}
+                            </Col>
+                        </Row>
+                    </Container>
+                </Jumbotron>
             </div>
 
         )
